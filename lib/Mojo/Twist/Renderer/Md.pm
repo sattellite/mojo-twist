b5df34ad7d5d024e16fb63b39a244940410f5a10
Alexander Groschev <sattellite@bks.tv>
2014-04-04 11:34:13 +0400

    Code tidy

--- lib/Mojo/Twist/Renderer/Md.pm
+++ lib/Mojo/Twist/Renderer/Md.pm
@@ -6,9 +6,9 @@ use Text::Markdown;
 sub render {
   my $self = shift;
   my ($md) = @_;
- 
-	my $parser = Text::Markdown->new;
-	my $output = $parser->markdown($md);
+
+  my $parser = Text::Markdown->new;
+  my $output = $parser->markdown($md);
 
   return $output;
 }
